b2a52284e219ffcbe941bf7d1a12d1ec1222fb98
Ed Adkins <ejmadkins@gmail.com>
2022-11-07 15:48:04 +0000

    add console.log

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const app = express();
 const storage = new Storage();
 app.use(express.json());
 
-const bucketName = 'ejmadkins-workstation-bucket'
+const bucketName = 'ejmadkins-workstation-bucket';
 
 app.get('/buckets', async (req, res) => {
-    try {
-        const [files] = await storage.bucket(bucketName).getFiles();
-        const fileResult = files.map((file) => file.name)
-        res.send(fileResult)
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
-    
+  try {
+    const [files] = await storage.bucket(bucketName).getFiles();
+    const fileResult = files.map((file) => file.name);
+    console.log(fileResult);
+    res.send(fileResult);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 module.exports = app;
