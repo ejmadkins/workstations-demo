24b92a3d648b7b068b3ba6d8b75238389f65b289
Ed Adkins <ejmadkins@gmail.com>
2022-11-24 08:32:53 +0000

    update path

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 const bucketName = 'ejmadkins-path-prod';
 
-app.get('/buckets', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     const [files] = await storage.bucket(bucketName).getFiles();
     const fileResult = files.map((file) => file.name);
